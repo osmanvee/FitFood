377e568f6c7bf39bd2ca89d69b935246b3758184
Osman Warsi <osman.warsi@live.com>
2020-05-20 00:02:59 -0400

    Final changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import React,{useEffect, useState} from 'react';
 import Recipe from './Recipe';
 import './App.css';
 import './index.js';
-import logomain from './logomain.png'
+import Burger from "./burger1.png";
+import s2 from "./b2.png";
+import s3 from "./b3.png";
+
 
 const App = () => {
  //Declaring constants to use later
@@ -16,37 +19,94 @@ const App = () => {
  //Passing array returned by function in this array we initialized
  const [recipes, SetRecipes] = useState([]);
  const [search, setSearch] = useState("");
-
+ const [query, setQuery] = useState("");
 
  
  
  useEffect(()=> {
   getRecipes();
- }, []);
+ }, [query]);
 
  //used to fetch our data
  const getRecipes = async () => {
   const resp = await fetch(
-    `https://api.edamam.com/search?q=peas&app_id=${APP_ID}&app_key=${APP_KEY}`
+    `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`
     );
   const data = await resp.json();
   SetRecipes(data.hits);
   console.log(data.hits);
 }
  
+//To change the search, takes in an event e
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    
+  };
+
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch('');
+  };
+
   return (
    
     
     <div className="App">
       {/** HEADER AREA */}
-      <h1>Grooli</h1>
-      <img src="./logomain.png"></img>
+      <div className="Header">
+        <div className="logo">
+          <h1><b>fit</b>food</h1>
+        </div>
+        <div className="github">
+          <p>find us:</p>
+          <a href="http://www.github.com/osmanvee">
+        <div className="git" ><i class="fab fa-github"></i></div>
+        </a>
+        <a href="http://www.instagram.com/osmanmedia">
+            <div className="insta"><i class="fab fa-instagram"></i></div>
+            </a>
+            <a href="http://www.facebook.com/osman.varsee">
+            <div className="fb"><i class="fab fa-facebook-f"></i></div>
+            </a>
+            
+        </div>
+      </div>
+      
+      {/** BANNER AREA */}
+      <div className="row">
+        <div className="column">
+          <img src={Burger} />
+          <div className="text">
+          Pick one main ingredient 
+        </div>
+        </div>
+        
+        <div className="column">
+        <img src={s2} />
+        <div className="text">
+          Input the ingredient, calories and other parameters.
+        </div>
+        </div>
+        <div className="column">
+        <img src={s3} />
+        <div className="text">
+          We will provide the best recipes for you!
+        </div>
+        </div>
+      </div>
 
+      
+     
       {/** INPUT AREA */}
-      <form className="search-form">
-        <input type="text" className="search-bar" value={search}/>
-        <button className="search-button" type="submit">Search</button>
+      <form onSubmit={getSearch} className="search-form">
+        
+        <input type="text" className="search-bar" placeholder="Search ingredient e.g 'Chicken'" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit"><i class="fas fa-search"></i></button>
+
       </form>
+
+      
 
       {recipes.map(recipe => (
         <Recipe 
@@ -54,8 +114,15 @@ const App = () => {
         title={recipe.recipe.label} 
         calories={recipe.recipe.calories}
         image={recipe.recipe.image}
+        ingredients={recipe.recipe.ingredients}
         />
       ))}
+
+      <div class="footer">
+        <p>
+          Powered by OsmanVee <i class="fab fa-osi"></i>
+        </p>
+      </div>
     </div>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import Navbar from 'react-bootstrap/Navbar';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
